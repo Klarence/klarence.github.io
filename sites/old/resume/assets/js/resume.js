dd6c1fc4226f1be2dd6693f7ad1eb4582b988d20
Klarence <ouyang.klarence@gmail.com>
2019-02-28 17:29:02 -0800

    updated old site directory

---
+++ sites/old/resume/assets/js/resume.js
@@ -0,0 +1,34 @@
+var getAge = function () {
+  var dateString = new Date("2010/1/1");
+
+  var today = new Date();
+  var workStartDate = new Date(dateString);
+  var diff = today-workStartDate;
+  var age = Math.floor(diff/31536000000);
+  // var age = today.getFullYear() - workStartDate.getFullYear();
+  // var m = today.getMonth() - workStartDate.getMonth();
+  // if (m < 0 || (m === 0 && today.getDate() < workStartDate.getDate())) {
+  //     age--;
+  // };
+  return age;
+}
+
+DomReady.ready(function() {
+	// alert('dom is ready');
+  document.getElementById("yearsExp").innerHTML = getAge();
+});
+
+
+// var birthdate = new Date("2009/1/1");
+// var cur = new Date();
+// var diff = cur-birthdate;
+// var age = Math.floor(diff/31536000000);
+// alert(age);
+
+	// var workExp = new Date(01/01/2009);//format 01/02/1900
+	//
+	// 	function calculateAge(workExp) { // workExp is a date
+	//     var ageDifMs = Date.now() - workExp.getTime();
+	//     var ageDate = new Date(ageDifMs); // miliseconds from epoch
+	//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+	// }
