4e7a1a466e6f3f1e94f6010d7941fca2a6a07c97
Klarence <Klarence.ux@gmail.com>
2024-12-28 11:20:20 -0600

    cleanup mainjs code
    simplify modals

--- js/main.js
+++ js/main.js
@@ -1,136 +1,22 @@
-'use strict';
-$(window).on('load', function() {
-	/*------------------
-		Preloader
-	--------------------*/
-	$(".loader").fadeOut();
-	$("#preloader").delay(400).fadeOut("slow");
+(() => {
+  const triggers = document.getElementsByClassName('modal-trigger');
+  const triggerArray = Array.from(triggers).entries();
+  const modals = document.getElementsByClassName('dialog');
+  const closeButtons = document.getElementsByClassName('btn-close');
 
-	if($('.portfolios-area').length > 0 ) {
-		const containerEl = document.querySelector('.portfolios-area');
-		const mixer = mixitup(containerEl);
-	}
-
-});
-
-
-(function($) {
-
-	/*------------------
-		Navigation
-	--------------------*/
-	// $('.nav-switch').on('click', function(event) {
-	// 	$('.main-menu').slideToggle(400);
-	// 	event.preventDefault();
-	// });
-
-
-
-	/*------------------
-		Background set
-	--------------------*/
-	$('.set-bg').each(function() {
-		const bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
-
-
-
-	/*----------------------
-		Portfolio layout
-	------------------------*/
-	const port_fi = $('.portfolios-area .first-item'),
-		port_si = $('.portfolios-area .second-item'),
-		port_intro_h = $('.portfolio-intro').innerHeight();
-
-	if ($(window).width() > 991) {
-		port_fi.appendTo('.portfolio-intro');
-		port_si.find('.portfolio-item').height(port_intro_h + 601);
-	}
-
-	$('.portfolio-item.pi-style2').each(function() {
-		const pi_width = $(this).width();
-		$(this).height(pi_width + 50);
-	});
-
-
-
-	/*------------------
-		Popup
-	--------------------*/
-	$('.portfolio-item').magnificPopup({
-		type: 'image',
-		mainClass: 'img-popup-warp',
-		removalDelay: 400,
-	});
-
-
-	/*------------------
-		Accordions
-	--------------------*/
-	$('.panel-link').on('click', function (e) {
-		$('.panel-link').parent('.panel-header').removeClass('active');
-		const $this = $(this).parent('.panel-header');
-		if (!$this.hasClass('active')) {
-			$this.addClass('active');
-		}
-		e.preventDefault();
-	});
-
-
-if($().circleProgress){
-
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 0.75,
-		size: 146,
-		thickness: 3,
-		fill: "#929292",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.83,
-		size: 146,
-		thickness: 3,
-		fill: "#929292",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 3
-	$("#progress3").circleProgress({
-		value: 0.25,
-		size: 146,
-		thickness: 3,
-		fill: "#929292",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 4
-	$("#progress4").circleProgress({
-		value: 0.95,
-		size: 146,
-		thickness: 3,
-		fill: "#929292",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-}
-
-    // var texts = [
-    //     "UX Engineer",
-    //     "Design Technologist",
-    //     "Usability Analyst",
-    //     "Accessibility Engineer",
-	// 	"Web Developer"
-    //     //, "Entrepreneur"
-    // ];
-    // var count = 0;
-	//
-    // function changeText() {
-    //     $("#whoAmI").text(texts[count]);
-    //     count < 3 ? count++ : count = 0;
-    // }
-	// // console.log("Ran");
-    // setInterval(changeText, 3000);
-
-})(jQuery);
-
-
+  for (const [index, trigger] of triggerArray) {
+    const openModal = () => {
+      modals[index].classList.add('show');
+      modals[index].showModal();
+    };
+    const closeModal = () => {
+      modals[index].classList.remove('show');
+      modals[index].close();
+    };
+    trigger.addEventListener('click', openModal);
+    closeButtons[index].addEventListener('click', (e) => {
+      e.preventDefault();
+      closeModal();
+    });
+  }
+})();
